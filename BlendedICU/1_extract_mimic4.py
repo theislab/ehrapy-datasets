9620fea96cab11d7a907fb0a1381927122a369d8
Andreas Gerardy <andreas.gerardy@proton.me>
2025-06-17 12:11:09 +0200

    Revert "cursor changes to paths, still need to test"
    This reverts commit 5e56ba866cfe2e096648b7ce1a8cecf851f8e1d6.

--- BlendedICU/1_extract_mimic4.py
+++ BlendedICU/1_extract_mimic4.py
@@ -11,17 +11,17 @@ Approximate running time:
 from mimic4_preprocessing.mimic4preparator import mimic4Preparator
 
 mimic4_prep = mimic4Preparator(
-    chartevents_pth='source/mimic4/icu/chartevents.csv.gz',                         
-    labevents_pth='source/mimic4/hosp/labevents.csv.gz',
-    d_labitems_pth='source/mimic4/hosp/d_labitems.csv.gz',
-    admissions_pth='source/mimic4/hosp/admissions.csv.gz',
-    diagnoses_pth='source/mimic4/hosp/diagnoses_icd.csv.gz',
-    d_diagnoses_pth='source/mimic4/hosp/d_icd_diagnoses.csv.gz',
-    d_items_pth='source/mimic4/icu/d_items.csv.gz',
-    inputevents_pth='source/mimic4/icu/inputevents.csv.gz',
-    outputevents_pth='source/mimic4/icu/outputevents.csv.gz',
-    icustays_pth='source/mimic4/icu/icustays.csv.gz',
-    patients_pth='source/mimic4/hosp/patients.csv.gz')
+    chartevents_pth='/icu/chartevents.csv.gz',                         
+    labevents_pth='/hosp/labevents.csv.gz',
+    d_labitems_pth='hosp/d_labitems.csv.gz',
+    admissions_pth='hosp/admissions.csv.gz',
+    diagnoses_pth='hosp/diagnoses_icd.csv.gz',
+    d_diagnoses_pth='hosp/d_icd_diagnoses.csv.gz',
+    d_items_pth='icu/d_items.csv.gz',
+    inputevents_pth='icu/inputevents.csv.gz',
+    outputevents_pth='icu/outputevents.csv.gz',
+    icustays_pth='icu/icustays.csv.gz',
+    patients_pth='hosp/patients.csv.gz')
 
 mimic4_prep.raw_tables_to_parquet()
 
